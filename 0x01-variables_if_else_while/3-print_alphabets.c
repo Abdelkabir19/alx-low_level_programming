d05a5cc1352cf7a9bd253fa5a1d6168bd7ecca9c
Abdelkabir19 <essalhia508@gmail.com>
2023-09-12 00:12:12 +0100

    hello

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	char alp[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$";
+	char alp[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 53; i++)
