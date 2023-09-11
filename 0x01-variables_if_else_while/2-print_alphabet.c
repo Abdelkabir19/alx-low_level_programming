a8364760ecf278e29a30bb6e5d2d23a2cb1ba230
Abdelkabir19 <essalhia508@gmail.com>
2023-09-11 23:57:36 +0100

    hello

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/*
-*main- fhfy
-*Return: dhyr
+/**
+* main - Fhfy
+*
+* Return: dhyr
 */
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
+
 	for (i = 0; i < 26; i++)
 	{
 	putchar(alp[i]);
