0aa6cc03bd9785ee0eb6bf84d35c3699bbce7e06
Abdelkabir19 <essalhia508@gmail.com>
2023-09-12 18:36:07 +0100

    hello c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - ñojviorhi
+*
+*Return: kughrukh
+*/
+int main(void)
+{
+	char put[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(put[i]);
+	}
+	putchar('\n');
+	return (0);
+}
