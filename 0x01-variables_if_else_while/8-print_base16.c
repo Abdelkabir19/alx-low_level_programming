105a0c2685b492f457c2be4c3e2e663103d99d66
Abdelkabir19 <essalhia508@gmail.com>
2023-09-12 01:10:29 +0100

    hello

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - jdju
+*
+*Return: fjjru
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i < '9' + 1; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i < 'g' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
