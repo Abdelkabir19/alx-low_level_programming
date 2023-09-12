a5788b402766e08642b804870edd9b426f051cf7
Abdelkabir19 <essalhia508@gmail.com>
2023-09-12 15:21:32 +0100

    hello c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+*main - iuhfiuhiuwh
+*
+*Return: hfiuwe
+*/
+int main(void)
+{
+	int i ,j;
+
+	for (i = '0' ; i < '9'; i++)
+	{
+		for (j = i + 1; j < '9' + 1; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return(0);
+}
