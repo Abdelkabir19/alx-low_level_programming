72971d224cabd57c41daea9c6f99a649418e49c8
Abdelkabir19 <essalhia508@gmail.com>
2023-09-12 15:52:43 +0100

    hello c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+*main - iufiu
+*
+*Return: oiejvioej
+*/
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i < '8'; i++)
+	{
+		for (j = i + 1; j < '9'; j++)
+		{
+			for (k = j + 1; k < '9' + 1; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
