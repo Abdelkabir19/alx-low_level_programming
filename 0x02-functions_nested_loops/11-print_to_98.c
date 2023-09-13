6282b91e584326672d4f40fc129413f03a7b719d
Abdelkabir19 <essalhia508@gmail.com>
2023-09-14 00:09:11 +0100

    hello

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+*print_to_98 - kdjgierj
+*
+*@n: iohjtj
+*Return: jiotrj
+*/
+void print_to_98(int n)
+{
+	if (n < 99)
+	{
+		for (i = n; i < 99; i++)
+		{
+			if (i < 0)
+			{
+				_putchar('-');
+				i *= -1;
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i > 97; i--)
+		{
+			if (i < 100)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			else
+			{
+				_putchar(i / 100 + '0');
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
