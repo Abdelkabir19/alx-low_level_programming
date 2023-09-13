01f4838024bd966720444063bdf0af5267ecb552
Abdelkabir19 <essalhia508@gmail.com>
2023-09-13 16:56:56 +0100

    hello

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*times_table - jhgehihg
+*
+*return: jdfhbuiehh
+*/
+void times_table(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(i * j + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+	}
+}
