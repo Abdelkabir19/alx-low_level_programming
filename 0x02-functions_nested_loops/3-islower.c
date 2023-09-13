7227cb0160756af1b8fa7c6e8c935ee7ff28dc34
Abdelkabir19 <essalhia508@gmail.com>
2023-09-13 09:49:12 +0100

    hello

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,14 @@
 #include "main.h"
 /**
+* _islower - iuhriueg
 *
-*islower - iuhriueg
-*
-*Return: jfjrur
+*@c: nfjrur
+* Return: jfjrur@c
 */
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
-	{
 		return (1);
-	} else 
-	{
+	else
 		return (0);
-	}
 }
