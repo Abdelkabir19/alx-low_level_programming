519c96a46e7e68eebe2d2c912b097198f563977d
Abdelkabir19 <essalhia508@gmail.com>
2023-09-14 22:01:01 +0100

    hello

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 /**
-* print_triangle - check the code
+* main - check the code
 *
-*@size: jkdhfweh
 * Return: Always 0.
 */
 int main(void)
 {
+	int i;
+
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0 )
 			printf("FizzBuzz");
-		else if (i % 3 = 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if (i % 5 = 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%d",i);
 	}
 	printf("\n");
+	return (0);
 }
