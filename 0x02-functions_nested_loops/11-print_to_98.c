1aecc0796d68903ac144b4611ecd329a05b3a40f
Abdelkabir19 <essalhia508@gmail.com>
2023-09-14 01:16:02 +0100

    hello

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,40 +14,17 @@ void print_to_98(int n)
 		for (i = n; i < 99; i++)
 		{
 			if (i > 9)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
+				_putchar(i / 10 + '0');_putchar(i % 10 + '0');
 			else if (i >= 0)
-			{
 				_putchar(i + '0');
-			}
-			else if ( i > -10)
-			{
-				r = i * -1;
-				_putchar('-');
-				_putchar(r + '0');
-			}
+			else if (i > -10)
+				r = i * -1;_putchar('-');_putchar(r + '0');
 			else if (i > -100)
-			{
-				r = i * -1;
-				_putchar('-');
-				_putchar(r / 10 + '0');
-				_putchar(r % 10 + '0');
-			}
+				r = i * -1;_putchar('-');_putchar(r / 10 + '0');_putchar(r % 10 + '0');
 			else
-			{
-				r = i * -1;
-				_putchar('-');
-				_putchar(r / 100 + '0');
-				_putchar((r / 10) % 10 + '0');
-				_putchar(r % 10 + '0');
-			}
+				r = i * -1;_putchar('-');_putchar(r / 100 + '0');_putchar((r / 10) % 10 + '0');_putchar(r % 10 + '0');
 			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				_putchar(',');_putchar(' ');
 		}
 	}
 	else
@@ -55,21 +32,12 @@ void print_to_98(int n)
 		for (i = n; i > 97; i--)
 		{
 			if (i < 100)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
+				_putchar(i / 10 + '0');_putchar(i % 10 + '0');
 			else
-			{
-				_putchar(i / 100 + '0');
-				_putchar((i / 10) % 10 + '0');
-				_putchar(i % 10 + '0');
-			}
+				_putchar(i / 100 + '0');_putchar((i / 10) % 10 + '0');_putchar(i % 10 + '0');
 			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				_putchar(',');_putchar(' ');
 		}
 	}
+	_putchar('\n')
 }
