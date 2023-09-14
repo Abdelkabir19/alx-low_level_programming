fbd05a95895af926e9512c63cc470ef8966e58a9
Abdelkabir19 <essalhia508@gmail.com>
2023-09-14 19:04:14 +0100

    hello

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_line - check the code
+*
+*@n: fnfgbjrj
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+	for (i = 0; i < n; i++)
+		putchar('-');
+	putchar('\n');
+}
