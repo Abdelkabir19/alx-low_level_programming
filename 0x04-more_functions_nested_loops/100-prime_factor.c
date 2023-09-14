f7f2adee891e61d597c6be81bdfc0cf1e705de40
Abdelkabir19 <essalhia508@gmail.com>
2023-09-14 23:20:15 +0100

    hello

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (i = 2; i < (612852475143 / 2 + 1); i++)
 	{
-		while (612852475143 % i = 0 )
+		while (612852475143 % i = 0)
 		{
 			i--;
 			j = i;
 		}
 	}
-	printf("%d",j);
+	printf("%d", j);
 	putchar('\n');
 }
