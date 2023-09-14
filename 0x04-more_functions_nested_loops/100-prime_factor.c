de778fc457d0dbfac752b5e382cee30f8868497c
Abdelkabir19 <essalhia508@gmail.com>
2023-09-14 23:18:34 +0100

    hello

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	for (i = 2; i < (612852475143 / 2 + 1); i++)
+	{
+		while (612852475143 % i = 0 )
+		{
+			i--;
+			j = i;
+		}
+	}
+	printf("%d",j);
+	putchar('\n');
+}
