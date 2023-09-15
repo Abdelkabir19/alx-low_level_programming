0faaa185d1b436513ff3e4a14984a8d83fe09871
Abdelkabir19 <essalhia508@gmail.com>
2023-09-15 16:42:07 +0100

    ijjgfr

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+* print_number - check the code
+*
+*@n: jghhr
+* Return: Always 0.
+*/
+void print_number(int n)
+{
+	int k, s, i, l = 1, r = 0;
+
+	if (n >= 0)
+	{
+		k = n;
+		s = k;
+	}
+	else
+	{
+		_putchar('-');
+		k = n * -1;
+		s = k;
+		n = k;
+	}
+	while (k >= 1)
+	{
+		k /= 10;
+		r++;
+		l *= 10;
+	}
+	l /= 10;
+	for (i = 1; i < r; i++)
+	{
+		n = (s / l) % 10;
+		_putchar(n + '0');
+		l /= 10;
+	}
+	_putchar(s % 10 + '0');
+}
