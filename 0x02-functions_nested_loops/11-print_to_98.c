d3a9e73561da639b2749755987546d0e1e9fa7c9
Abdelkabir19 <essalhia508@gmail.com>
2023-09-15 10:31:22 +0100

    c fonction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,41 +6,25 @@
 */
 void print_to_98(int n)
 {
-	int i, j, k;
+	int i;
 
-	if (n >= 0 && n <= 15)
+	if (n <= 98)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = n; i <= 98; i++)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j*i;
-				if (j == 0)
-					_putchar(k + '0');
-				else if(k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10)+'0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	putchar('\n');
 }
