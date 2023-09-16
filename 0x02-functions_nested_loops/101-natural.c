f95575a342a089d70085243df602dd85d007c72f
Abdelkabir19 <essalhia508@gmail.com>
2023-09-16 16:20:30 +0100

    hello c programming

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,18 +6,15 @@
 */
 int main(void)
 {
-	int i, k = 0;
+	int i, s, k = 0;
 
 	for (i = 1; i < 342; i++)
 	{
 		if (i % 5 != 0)
-		{
 			k += i * 3;
-			while (5 * i < 1024)
-				k += i * 5;
-		}
-		else
-			k += i * 5;
+		s = i * 5;
+		if (s < 1024)
+			k += s;
 	}
 	printf("%d\n", k);
 	return (0);
