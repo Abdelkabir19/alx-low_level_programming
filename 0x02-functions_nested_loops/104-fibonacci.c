ee0bb30c7154048a309a32c069884797d8c69ca5
Abdelkabir19 <essalhia508@gmail.com>
2023-09-17 17:52:14 +0100

    finish

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,15 +6,33 @@
 */
 int main(void)
 {
-	long double k, i, s = 0, n = 1;
+	unsigned long long int k, i, a = 0, s = 0, n = 1;
 
 	for (i = 0; i < 98; i++)
 	{
-		k = s + n;
-		s = n;
-		n = k;
-		printf("%0.0Lf, ", k);
+		if (i > 90)
+		{
+			a += s % 100 + n % 100;
+			s /= 100;
+			n /= 100;
+			k = s + n;
+			s = n;
+			n = k;
+			k += a / 100;
+			a %= 100;
+			if (i != 97)
+				printf("%llu%llu, ", k, a);
+			else
+				printf("%llu%llu\n", k, a);
+		} else
+		{
+			k = s + n;
+			s = n;
+			n = k;
+			if (i != 97)
+				printf ("%llu, ", k);
+		}
 	}
-	printf("%0.0Lf, ", k);
 	return (0);
+	
 }
