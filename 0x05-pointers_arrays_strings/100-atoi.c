906037f58c0c57a306d955ebc79fc678f293453f
Abdelkabir19 <essalhia508@gmail.com>
2023-09-19 19:56:49 +0100

    hello c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi- check the code
+ *
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	int k = 1, j = 0, i, c = 0, l = 1;
+
+	while ((*s < '0' || *s > '9') && (*s != '\0'))
+		s++;
+	if (*s != '\0')
+	{
+		s--;
+		if (*s == '-')
+			k = -1;
+		s++;
+		while (*s > '0' && *s < '9')
+		{
+			s[j] = *s;
+			j++;
+		}
+		s[j] = '\0';
+		for (i = j - 1; i >= 0; i++)
+		{
+			c += s[i] * l;
+			l *= 10;
+		}
+	} else
+		c = 0;
+	return(k * c)
+}
