b7464b4d6edd18ea77b184014f9e9fa9a67cb15b
Abdelkabir19 <essalhia508@gmail.com>
2023-09-20 14:36:21 +0100

    hello c pointer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int i, j = 0;
+
 	while (*dest != '\0')
+	{
 		dest++;
+		j++;
+	}
 	while (*src != '\0')
 	{
 		*dest = *src;
 		src++;
 		dest++;
+		j++;
 	}
 	*dest = '\0';
+	for (i = 0; i < j; i++)
+		dest--;
 	return (dest);
 }
