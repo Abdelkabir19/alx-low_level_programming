86457ce4244935f4c70da77ab1d45d02b5f2a699
Abdelkabir19 <essalhia508@gmail.com>
2023-09-21 15:37:37 +0100

    that's what i call it pointer

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - check the code
+ *
+ *
+ *@a: gioje
+ * Return: Always 0.
+ */
+char *cap_string(char *)
+{
+	int n;
+
+	while (*a != '\0')
+	{
+		if (*a == ' ' || *a == '\t' || *a == '\n' || *a == ',' || *a == '.' || *a == ';' || *a == '!' || *a == '?' || *a == '"' || *a == '(' || *a == ')' || *a == '{' || *a == '}')
+		{
+			a++;
+			if (*a < 'z' && *a > 'a')
+				*a += 'A' - 'a';
+			else
+				a--;
+		}
+		a++;
+		n++;
+	}
+	while (n > 0)
+	{
+		a--;
+		n--;
+	}
+	return (a);
+}
