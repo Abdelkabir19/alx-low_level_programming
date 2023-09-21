2b9cf4a19f9c22153be2e40af5b4c4d07fd89773
Abdelkabir19 <essalhia508@gmail.com>
2023-09-21 14:37:23 +0100

    that's what i call it pointer

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int c;
 
-	c = *s1 - *s2;
-	if (c < 0)
-		c *= -1;
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	if (*s2 != '\0')
-		c *= -1;
+	c = *s1 - *s2;
 	return (c);
 }
