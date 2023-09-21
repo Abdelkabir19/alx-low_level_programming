3ce407bc5c0201f76248f29e14420d50d93b14f7
Abdelkabir19 <essalhia508@gmail.com>
2023-09-22 00:02:12 +0100

    this what i be call it pointer

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *leet- check the co<de for
+ *
+ *@a: jjiojifdb
+ * Return: Always 0.
+ */
+char *leet(char *a)
+{
+	int n;
+	char *s = "aAeEoOtTlL";
+	char *c = "4433007711";
+
+	while (*a != '\0')
+	{
+		while (*s != '\0')
+		{
+			if (*a == *s)
+				*a = *c;
+			s++;
+			c++;
+		}
+		a++;
+		n++;
+	}
+	a -= n;
+}
+
