74acde86f49c59d6cb3a4afb7e46813d9b9ff7fe
Abdelkabir19 <essalhia508@gmail.com>
2023-09-25 22:22:00 +0100

    hello another time pointer

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * _strchr - check the code
  *
+ *@c: fmmmk
+ *@s: jmorj
  * Return: Always 0.
  */
 char *_strchr(char *s, char c)
 {
-
 	while (*s != c)
 		s++;
+	if (*s == '\0')
+		s = NULL;
 	return (s);
 }
