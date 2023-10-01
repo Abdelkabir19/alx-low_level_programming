e6ce929f991518c352261e2910e4c8c9b0a27e39
Abdelkabir19 <essalhia508@gmail.com>
2023-10-01 23:35:03 +0100

    hello recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,19 +11,17 @@ int _sqrt_recursion(int n)
 
 	if (n < 0)
 		return (-1);
-	if (n == 0)
+	else if (n == 0)
 		return (0);
-	if (n == 1)
+	else if (n == 1)
 		return (1);
 	if (i * i < n)
 	{
 		i++;
 		i += _sqrt_recursion(n);
-	} else
-	{
-		if (i * i == n)
-			return (i);
-		else
-			return (-1);
 	}
+	i--;
+	if (i * i != n)
+		i = -1;
+	return (i);
 }
