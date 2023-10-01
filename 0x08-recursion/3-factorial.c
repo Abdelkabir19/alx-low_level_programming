c9b3826616c19942afcf7daab5d82c95d2a8f52b
Abdelkabir19 <essalhia508@gmail.com>
2023-10-01 12:12:44 +0100

    hello recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,11 +3,14 @@
 /**
  * factorial - check the code
  *
+ *@n: iojirt
  * Return: Always 0.
  */
 int factorial(int n)
 {
-	if (n <= 1)
+	if (n < 0)
+		return (-1);
+	else if (n <= 1)
 		return (1);
 	return (n * factorial(n - 1));
 }
