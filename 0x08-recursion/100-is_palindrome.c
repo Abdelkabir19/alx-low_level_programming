a9d76675858ec93b2484b4afc6b2c3d4c7158906
Abdelkabir19 <essalhia508@gmail.com>
2023-10-03 16:02:20 +0100

    smart

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -21,7 +21,7 @@ int ak(char *s, int i, int n)
 {
 	if (*s != '\0' && i == 0)
 		ak(s + 1, i, n + 1);
-	else if (s[i] == s[n - 1 - i] && i + 1 <= n / 2)
+	else if (s[i] == s[n - 1 - i] && i + 1 < n / 2)
 		ak(s, i + 1, n);
 	if (i == n / 2)
 		return (1);
