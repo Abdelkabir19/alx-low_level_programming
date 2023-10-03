8497c42ba6d11060968ae3c7684b480be1fe3662
Abdelkabir19 <essalhia508@gmail.com>
2023-10-03 16:16:23 +0100

    smart

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -19,11 +19,17 @@ int is_palindrome(char *s)
   */
 int ak(char *s, int i, int n)
 {
+	int j;
+
 	if (*s != '\0' && i == 0)
 		ak(s + 1, i, n + 1);
-	else if (s[i] == s[n - 1 - i] && i + 1 < n / 2)
+	if (n % 2)
+		j = n;
+	else
+		j = n + 1;
+	else if (s[i] == s[n - 1 - i] && i + 1 < j / 2)
 		ak(s, i + 1, n);
-	if (i == n / 2 - 1)
+	if (i == j / 2)
 		return (1);
 	else
 		return (0);
