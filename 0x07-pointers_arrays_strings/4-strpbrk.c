4c7cf543e23ba98976bcf1b37510742a5d838ab5
Abdelkabir19 <essalhia508@gmail.com>
2023-10-04 21:37:01 +0100

    ttttt

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,15 +8,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
+	int i = 0;
+
 	while (*s != '\0')
 	{
 		while (*accept != '\0')
 		{
 			if (*s == *accept)
 				return (s);
-			accept++;
+			i++;
+			accept[i];
 		}
-		accept = accept[0];
+		i = 0;
 		s++;
 	}
+	return ('\0');
 }
