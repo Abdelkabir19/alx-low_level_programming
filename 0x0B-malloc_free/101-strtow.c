33375f0f1a80d77e709b75b4ed43e2adbd395231
Abdelkabir19 <essalhia508@gmail.com>
2023-10-07 12:52:45 +0100

    wow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -21,6 +21,7 @@ int wcont(char *s)
 		else if (i == 0)
 			n++;
 	}
+	n++;
 	return (n);
 }
 /**
@@ -33,24 +34,27 @@ char **strtow(char *str)
 {
 	int i = 0, j, k = 0, l, m, n;
 	char **o;
-
+	
 	n = wcont(str);
-	if (n == 0)
+	if (n == 1)
 		return (NULL);
-	o = (char **)malloc(sizeof(char) * n + 1);
+	o = (char **)malloc(sizeof(char *) * n);
 	if (o == NULL)
 		return (NULL);
-	while (str[i] != '\0')
+	o[n - 1] = NULL;
+	while (str[i])
 	{
-		if (str[i] == ' ' && (i == 0  || str[i + 1] != ' '))
+		if (str[i] != ' ' && (i == 0  || str[i - 1] == ' '))
 		{
-			for (j = 1; str[i + j] != ' ' || str[i + j] != ' '; j++)
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 				;
-			o[k] = (char *)malloc(sizeof(char) * j + 1);
+			j++;
+			o[k] = (char *)malloc(sizeof(char) * (j + 1));
 			if (o[k] == NULL)
 			{
 				for (m = 0; m < k; m++)
 					free(o[m]);
+				free(o[n - 1]);
 				free(o);
 				return (NULL);
 			}
