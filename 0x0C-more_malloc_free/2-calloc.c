8b5c62f16f50dd6eeb57a05a17038f1d92f5bfb3
Abdelkabir19 <essalhia508@gmail.com>
2023-10-08 23:27:04 +0100

    wow

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -36,6 +36,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	m = malloc(sizeof(int) * nmemb);
 	if (m == 0)
 		return (NULL);
-	_memset(m, 0,sizeof(int) * nmemb);
+	_memset(m, 0, sizeof(int) * nmemb);
 	return (m);
 }
