d2ac4b57d090c20c9cb03d2c9d75ad084ba5d76e
Abdelkabir19 <essalhia508@gmail.com>
2023-10-08 22:15:05 +0100

    wow

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,8 +25,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str = malloc(s1len + n + 1);
 	if (str == NULL)
 		return(NULL);
-	for (i = 0; s[i];i++)
-		str[i] = s[i];
+	for (i = 0; s1[i];i++)
+		str[i] = s1[i];
 	for (j = 0; j < n; j++)
 	{
 		str[i] = s2[j];
