74ac12e17318fb0fc191dadde2cc4a25e3b54f0b
Abdelkabir19 <essalhia508@gmail.com>
2023-10-10 23:47:33 +0100

    hello macro

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,18 +7,16 @@
   *@age: kjjh
   *@owner: khnn
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
-
-
 
 
 
