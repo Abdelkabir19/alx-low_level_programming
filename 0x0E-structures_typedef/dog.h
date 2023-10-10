75a3731f463ee3005ba7507aeca4c04d8461c03d
Abdelkabir19 <essalhia508@gmail.com>
2023-10-10 23:07:14 +0100

    hello macro

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
-	char* name;
+/**
+  *struct dog - nnj
+  *
+  *@name: mokjy
+  *@age: kjjh
+  *@owner: khnn
+  */
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 #endif
