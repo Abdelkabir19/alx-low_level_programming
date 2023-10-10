4e3f0d080aa60b7717e8214950b6c0724da4b6f2
Abdelkabir19 <essalhia508@gmail.com>
2023-10-10 23:36:41 +0100

    hello macro

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - check the code
+ *
+ *@d: lmh
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if(d)
+	{
+		printf("Name: %s\n",d->name ? d->name :"(nil)");
+		printf("Age: %f\n",d->age);
+		printf("owner: %s\n",d->owner ? d->owner : "(nil)");
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,14 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
+void print_dog(struct dog *d);
+
+
+
+
+
+
+
 
 
 #endif
