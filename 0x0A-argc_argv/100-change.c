545cadd2d9a80fb4eb5481cbfae3784c454c3353
Abdelkabir19 <essalhia508@gmail.com>
2024-01-19 16:26:49 +0100

    with you can know the amount

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	int a, b, c, d, m, s;
 
 	if (argc != 2)
-		return(printf("Error\n"), 1);
+		return (printf("Error\n"), 1);
 	s = atoi(argv[1]);
 	if (s <= 0)
 		printf("0\n");
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 		b = (s - a * 25) / 10;
 		c = (s - a * 25 - b * 10) / 5;
 		d = (s - a * 25 - b * 10 - c * 5) / 2;
-		m = a + b + c + d + s - a * 25 - b * 10 - c * 5 - d * 2 ;
+		m = a + b + c + d + s - a * 25 - b * 10 - c * 5 - d * 2;
 		printf("%d\n", m);
 	}
 	return (0);
