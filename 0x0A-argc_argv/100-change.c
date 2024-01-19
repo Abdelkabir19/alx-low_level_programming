6e8dee17bf112abe08b8733f4559cc70665a585b
Abdelkabir19 <essalhia508@gmail.com>
2024-01-19 16:25:18 +0100

    with you can know the amount

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - ngiu
+*@argc: knjtr
+*@argv: ijijtr
+*
+*Return: mkjimm
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, c, d, m, s;
+
+	if (argc != 2)
+		return(printf("Error\n"), 1);
+	s = atoi(argv[1]);
+	if (s <= 0)
+		printf("0\n");
+	else
+	{
+		a = s / 25;
+		b = (s - a * 25) / 10;
+		c = (s - a * 25 - b * 10) / 5;
+		d = (s - a * 25 - b * 10 - c * 5) / 2;
+		m = a + b + c + d + s - a * 25 - b * 10 - c * 5 - d * 2 ;
+		printf("%d\n", m);
+	}
+	return (0);
+}
