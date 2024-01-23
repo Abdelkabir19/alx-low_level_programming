f6ffc89474fdee529a330d2c692b8688a75a5b3e
Abdelkabir19 <essalhia508@gmail.com>
2024-01-23 23:54:14 +0100

    all are good

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
-*stelen - a function that gets a length of string
+* _strlen - a function that gets a length of string
 *@str: the string to get the length
 *Return: length of astr
 */
@@ -15,10 +15,10 @@ int _strlen(const char *str)
 	return (length);
 }
 /**
-* strcopy - a function that returns odest with a copy of a string from Osrc
+* _strcopy - a function that returns odest with a copy of a string from Osrc
 * @src: string to copy
 * @dest: copy string to here
-* Return : dest
+* Return: dest
 */
 char *_strcopy(char *dest, char *src)
 {
