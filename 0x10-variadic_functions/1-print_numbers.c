a56cc83f65e53d59e9c21ce4f3e03a0405cbc15e
Abdelkabir19 <essalhia508@gmail.com>
2024-01-25 19:14:09 +0100

    you can add what you be

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+*print_numbers -  prints numbers, followed by a new line. and separated by a seperator
+*@separator:  is the string to be printed between numbers
+*@n: is the number of integers passed to the function
+*@...: the number will printed
+*
+*Return: ¿¿¿
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list ptr;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ptr, n);
+	while (i--)
+		printf("%d%s", va_arg(ptr, int), i ? (separator ? separator : "") : "\n");
+	va_end (ptr);
+}
+
