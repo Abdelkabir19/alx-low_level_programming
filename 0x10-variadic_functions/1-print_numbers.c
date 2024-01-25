ea5e136d7e027767f6a3872e178ebe19fabbb48b
Abdelkabir19 <essalhia508@gmail.com>
2024-01-25 19:39:27 +0100

    you can add what you be

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 /**
-*print_numbers -  prints numbers, followed by a new line. and separated by a seperator
+*print_numbers -  prints numbers, followed by a new line.
 *@separator:  is the string to be printed between numbers
 *@n: is the number of integers passed to the function
 *@...: the number will printed
@@ -20,6 +20,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ptr, n);
 	while (i--)
 		printf("%d%s", va_arg(ptr, int), i ? (separator ? separator : "") : "\n");
-	va_end (ptr);
+	va_end(ptr);
 }
 
